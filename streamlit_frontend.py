98903a37b6a10ff98da0b01092b08f8afcc26b3e
Manoj Kumar <manojmahendia@gmail.com>
2026-04-18 06:21:42 +0530

    test

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -1,19 +1,22 @@
 import streamlit as st
 
+if 'message_history' not in st.session_state:
+    st.session_state['message_history']= []
+
 
 message_history = []
 
-for message in message_history:
+for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
         st.text(message['content'])
 user_input = st.chat_input('Type here')
 
 
 if user_input:
-    message_history.append({'role': 'user', 'content': user_input})
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
     with st.chat_message('user'):
         st.text(user_input)
     
-    message_history.append({'role': 'user', 'content': user_input})
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
     with st.chat_message('assistant'):
         st.text(user_input)
