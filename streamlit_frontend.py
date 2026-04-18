fef2b0284ef41855ea6f43e23dafed75cd796153
Manoj Kumar <manojmahendia@gmail.com>
2026-04-18 06:17:17 +0530

    test
    quit
    exit

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -1,7 +1,19 @@
 import streamlit as st
 
+
+message_history = []
+
+for message in message_history:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
 user_input = st.chat_input('Type here')
 
+
 if user_input:
+    message_history.append({'role': 'user', 'content': user_input})
     with st.chat_message('user'):
+        st.text(user_input)
+    
+    message_history.append({'role': 'user', 'content': user_input})
+    with st.chat_message('assistant'):
         st.text(user_input)
