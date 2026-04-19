605a7c7ad0885d64aab6bb8f356e4e0433f69d84
Manoj Kumar <manojmahendia@gmail.com>
2026-04-19 07:35:22 +0530

    test

--- streamlit_frontend_threading.py
+++ streamlit_frontend_threading.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from langgraph_backend import chatbot
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, AIMessage
+import uuid
+
+
 
 
 # sidebar UI section
@@ -11,6 +14,24 @@ if st.sidebar.button('New Chat'):
 
 st.sidebar.header('My Conversations')
 
+# creating dynamic thread id
+
+def generate_thread_id():
+    thread_id = uuid.uuid4()
+    return thread_id
+
+def reset_chat():
+    thread_id = generate_thread_id()
+    st.session_state['thread_id']=thread_id
+    add_thread(st.session_state['thread_id'])
+
+    #clear message state for clear ui
+    st.session_state['message_history'] = []
+
+if 'chat_threads' not in st.session_state:
+    st.session_state['chat_threads']=[]
+
+
 
 
 # st.session_state -> dict -> 
@@ -18,6 +39,9 @@ CONFIG = {'configurable': {'thread_id': 'thread-1'}}
 
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
+
+if 'thread_id' not in st.session_state:
+    st.session_state['message_history'] = generate_thread_id()
 
 # loading the conversation history
 for message in st.session_state['message_history']:
