2ca18e3b3ede69b842df5d7bd3ac08593e6ef74a
Manoj Kumar <manojmahendia@gmail.com>
2026-04-20 07:46:55 +0530

    test

--- streamlit_frontend_streaming.py
+++ streamlit_frontend_streaming.py
@@ -5,8 +5,15 @@ from langchain_core.messages import HumanMessage
 # st.session_state -> dict -> 
 CONFIG = {'configurable': {'thread_id': 'thread-1'}}
 
+#********************************************* Session Setup *********************************************
+
+
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
+
+#********************************************* Sidebar UI ***********************************************
+
+#********************************************* Main UI **************************************************
 
 # loading the conversation history
 for message in st.session_state['message_history']:
